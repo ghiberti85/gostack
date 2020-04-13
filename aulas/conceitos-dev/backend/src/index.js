1e8d5670c51896d095c5c39847f4399a6e2ec886
Fernando Ghiberti <ghiberti@Fernandos-MacBook-Pro.local>
2020-04-13 20:23:42 -0300

    Add Desafio 3: Conceitos ReactJS

--- aulas/conceitos-dev/backend/src/index.js
+++ aulas/conceitos-dev/backend/src/index.js
@@ -1,8 +1,10 @@
 const express = require ('express');
+const cors = require ('cors');
 const { uuid, isUuid } = require('uuidv4'); //lib para criação do ID - Universal Unique ID 
 
 const app = express();
 
+app.use(cors());
 app.use(express.json()); //Precisa vir ANTES das rotas.
 
 /**
